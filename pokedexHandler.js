ebeccae3a77712825442ace81ef10b838f65933b
Skylldra <kily1010mc@gmail.com>
2025-01-26 20:54:25 +0100

    Update pokedexHandler.js

--- pokedexHandler.js
+++ pokedexHandler.js
@@ -1,19 +1,17 @@
 const fetch = require('node-fetch');
 
-// Bestehende Funktion
 async function sendToPokedex(username, pokemon, isCaught, isShiny) {
     const pokedexUrl = `https://pokedex-dt48.onrender.com/catch`;
 
     try {
-        // Daten an das Pokédex-Backend senden
         const response = await fetch(pokedexUrl, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 username: username,
                 pokemonId: pokemon.id,
-                caught: isCaught,
-                shiny: isShiny
+                caught: isCaught === '◓Gefangen◓',
+                shiny: !!isShiny
             })
         });
 
@@ -24,6 +22,8 @@ async function sendToPokedex(username, pokemon, isCaught, isShiny) {
         throw new Error('Fehler beim Kommunizieren mit dem Pokédex.');
     }
 }
+
+module.exports = { sendToPokedex };
 
 // Neue Funktion, die zusätzlich genutzt werden kann
 async function handlePokedexInteraction(username, pokemon, isCaught, isShiny) {
