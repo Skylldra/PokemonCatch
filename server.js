45e478b865eb6a29f813e7e983da0b4091f58d03
Skylldra <kily1010mc@gmail.com>
2025-03-19 22:00:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -84,7 +84,8 @@ const pokeballs = {
 // Format: "twitch_username": "Pokeball-Typ"
 const specialBallUsers = {
     // Beispiele:
-    "Kampfschein90": "Superball",
+    "Kampfschwein90": "Superball",
+    "kampfschwein90": "Superball",
     "beispieluser2": "Hyperball",
     // Füge hier weitere Spieler mit Sonderberechtigung hinzu
 };
